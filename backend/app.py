b9868f63e1c68cbc842e5a3098a3d3982d683c54
vseviln <vasco.sevil@utec.edu.pe>
2023-11-16 13:40:42 -0500

    a

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ from uuid import uuid4
 app = Flask(__name__)
  
 app.config['SECRET_KEY'] = 'matias123'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://vseviln:Vasena0400@vseviln.mysql.pythonanywhere-services.com/default'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://vseviln:Vasena0400@vseviln.mysql.pythonanywhere-services.com/vseviln$default'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
   
