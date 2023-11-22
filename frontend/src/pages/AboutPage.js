06df686536bd151ba9fd74140a320e79576c0c43
vseviln <vasco.sevil@utec.edu.pe>
2023-11-22 17:55:00 -0500

    about

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import myImage from './src/images/vynil.png'; 
 
 
 const About = () => {
@@ -9,7 +8,6 @@ const About = () => {
       <p>
         Trackup is a revolutionary platform for music enthusiasts. It allows users to discover new music, create playlists, and connect with artists.
       </p>
-      <img src={myImage} alt="vynil" />
     </div>
   );
 };
